07a58d19f3ad3cad80bd8fa166b280de89a572dd
Dip Gurung <deepgrg03@gmail.com>
2020-02-14 20:33:23 +0545

    The template for web is created

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context={}
+    return render(request,'accounts/index.html',context)
+
+def products(request):
+    context={}
+    return render(request,'accounts/products.html',context)
+
+def customer(request):
+    context={}
+    return render(request,'accounts/customer.html',context)
